9b3d8de94da7b7b60544837827b1678ef7f0d29d
Hiroya Yabiku <h_yabiku@atoj.co.jp>
2022-02-05 13:49:14 +0900

    不要な記述削除

--- django/api/settings.py
+++ django/api/settings.py
@@ -33,14 +33,6 @@ ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SECRET_KEY
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
 
 # Application definition
 
